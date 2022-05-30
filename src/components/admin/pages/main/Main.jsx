16ca81dfb20451894086b85ecc493b69d3acede4
Azizbek1 <46714958+Azizbek1@users.noreply.github.com>
2022-05-30 15:55:14 +0500

    sadas
    dasdsadsaasdsa

--- src/components/admin/pages/main/Main.jsx
+++ src/components/admin/pages/main/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../../utils/navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-            <h1>Main</h1>
+            <Navbar/>
         </div>
     );
 }
