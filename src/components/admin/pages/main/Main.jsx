db511a045baf6e5d73f84c5e8fd0dc1e8cd7d57a
Azizbek1 <46714958+Azizbek1@users.noreply.github.com>
2022-06-01 10:51:35 +0500

    sdadasdsa
    dasdasda

--- src/components/admin/pages/main/Main.jsx
+++ src/components/admin/pages/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../utils/navbar/Navbar';
+import Sidebar from '../../utils/sidebar/Sidebar';
 
 const Main = () => {
     return (
         <div>
             <Navbar/>
+            <Sidebar />
         </div>
     );
 }
