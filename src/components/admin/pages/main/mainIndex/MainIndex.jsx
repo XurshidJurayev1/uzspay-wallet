4b42ff2dabdde1f6b085774bf15a9a4cee666892
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-06-21 12:28:50 +0500

    response main mainCard

--- src/components/admin/pages/main/mainIndex/MainIndex.jsx
+++ src/components/admin/pages/main/mainIndex/MainIndex.jsx
@@ -10,19 +10,26 @@ const MainIndex = () => {
       <div className="mainindex">
         <div className="container__main">
           <h2 className="mainindex__title-p">Платежи и переводы</h2>
-          <div className="row justify-content-between  flex-wrap ">
-            <div className="col-md-3">
+          <div className="row justify-content-between  mainindex-flex-wrap ">
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
               <Maincard />
             </div>
-            <div className="col-md-3">
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
               <Maincard />
             </div>
-            <div className="col-md-3">
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
               <Maincard />
             </div>
-            <div className="col-md-3">
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
               <Maincard />
             </div>
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
+              <Maincard />
+            </div>
+            <div className="col-md-3 mainindex-flex-wrap-col-md">
+              <Maincard />
+            </div>
+
           </div>
           <div className="row mt-3">
             <div className="col-md-4">
