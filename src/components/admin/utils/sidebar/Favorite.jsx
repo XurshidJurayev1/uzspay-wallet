7479bd1d42e50d0fa2d7b90c7682851983e14c6f
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-06-22 18:54:52 +0500

    payment page response

--- src/components/admin/utils/sidebar/Favorite.jsx
+++ src/components/admin/utils/sidebar/Favorite.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Fovoritemini from './Fovoritemini';
-import "./sidebar.scss";
+import './sidebar.scss';
+
 const Favorite = () => {
-    return (
-        <div className="favorite">
-            <h3 className='favorite__title'>Избранное</h3>    
-            <Fovoritemini />
-            <Fovoritemini />
-            <Fovoritemini />
-        </div>
-    );
-}
+  return (
+    <div className="favorite">
+      <h3 className="favorite__title">Избранное</h3>
+      <div className="favorite_mobile_scroll">
+        <Fovoritemini />
+        <Fovoritemini />
+        <Fovoritemini />
+        <Fovoritemini />
+        <Fovoritemini />
+        <Fovoritemini />
+      </div>
+
+    </div>
+  );
+};
 
 export default Favorite;
