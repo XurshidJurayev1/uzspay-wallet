9e681d44b8eedae26c6bf1c4e8d092031b70e9a5
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-06-22 16:08:01 +0500

    sidebar fixed overflow scroll

--- src/components/admin/utils/sidebar/Accardion.jsx
+++ src/components/admin/utils/sidebar/Accardion.jsx
@@ -1,23 +1,24 @@
-import React , { useState } from 'react';
-import uzb from '../../../../assets/images/uzb_Ra.png'
-import card from '../../../../assets/images/card.png'
-import down from '../../../../assets/images/caretdown 1.png'
+import React, { useState } from 'react';
+import uzb from '../../../../assets/images/uzb_Ra.png';
+import card from '../../../../assets/images/card.png';
+import down from '../../../../assets/images/caretdown 1.png';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Acar from './Acar';
+
 const Accardion = () => {
-    const [isActive, setIsActive] = useState("title1");
-    
-    return (
-        <div>
-            <div className="sidebar__acardion">
-                <h2>Мои карты</h2>
-                <Acar title={"title1"} active={isActive} setIsActive={setIsActive} />
-                <Acar title={"title2"} active={isActive} setIsActive={setIsActive} />
-                <Acar title={"title3"} active={isActive} setIsActive={setIsActive} />
-               
-            </div>
-        </div>
-    );
-}
+  const [isActive, setIsActive] = useState('title1');
+
+  return (
+    <div>
+      <div className="sidebar__acardion">
+        <h2>Мои карты</h2>
+        <Acar />
+        <Acar />
+        <Acar />
+
+      </div>
+    </div>
+  );
+};
 
 export default Accardion;
