13312fd780b2c95e41f4998c6a8d01650830b840
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-06-27 17:22:13 +0500

    send money page

--- src/components/admin/pages/transMoney/sendMoney/SendMoney.jsx
+++ src/components/admin/pages/transMoney/sendMoney/SendMoney.jsx
@@ -3,11 +3,16 @@ import './SendMoney.scss';
 import security from '../../../../../assets/svg/security1.svg';
 import security2 from '../../../../../assets/svg/security2.svg';
 import { ReactComponent as Close } from '../../../../../assets/svg/close2.svg';
+import { ReactComponent as Close2 } from '../../../../../assets/svg/closeInput.svg';
+import { ReactComponent as Caretdown } from '../../../../../assets/svg/caretdown1.svg';
 import InputMask from 'react-input-mask';
-
+import kb from '../../../../../assets/svg/notFoundCard.svg';
 
 const SendMoney = () => {
   const [sum, setSum] = useState('');
+  const [cardNum, setCardNum] = useState('');
+  const [validity, setValidity] = useState('');
+  const [cvc, setCvc] = useState('');
   return (<div className="send-money">
     <div className="send-money-container">
       <h2 className="send-money-container-title">
@@ -33,8 +38,53 @@ const SendMoney = () => {
           С карты
         </h3>
         <div className="send-money-container-content-input-card">
-
+          <div className="send-money-container-content-input-card-input">
+            <div className="send-money-container-content-input-card-input-svg"
+                 style={{ backgroundImage: `url(${kb})` }} />
+            <InputMask
+              placeholder="0000 0000 0000 0000"
+              maskChar={null}
+              mask="9999 9999 9999 9999"
+              className="send-money-container-content-input-card-input-input"
+              value={cardNum}
+              onChange={(e) => setCardNum(e.target.value)}
+            />
+            <label className="send-money-container-content-input-card-input-label">Номер карты</label>
+          </div>
+          <div className="send-money-container-content-input-card-validity">
+            <InputMask
+              maskChar={null}
+              mask="99/99"
+              placeholder="00/00"
+              value={validity}
+              onChange={(e) => setValidity(e.target.value)}
+              className="send-money-container-content-input-card-validity-input"
+            />
+            <label className="send-money-container-content-input-card-validity-label">Срок действия</label>
+          </div>
+          <div className="send-money-container-content-input-card-validity send-money-container-content-input-card-cvc "
+               style={{ right: '200px' }}>
+            <InputMask
+              maskChar={null}
+              mask="999"
+              placeholder="cvc"
+              value={cvc}
+              onChange={(e) => setCvc(e.target.value)}
+              className="send-money-container-content-input-card-validity-input"
+            />
+            {/*<label className="send-money-container-content-input-card-validity-label">CVC</label>*/}
+          </div>
+          <div className="send-money-container-content-input-card-close" onClick={() => {
+            setValidity('');
+            setCardNum('');
+          }}>
+            <Close2 />
+          </div>
+          <div className="send-money-container-content-input-card-collapse">
+            <Caretdown />
+          </div>
         </div>
+
         <h3 className="send-money-container-content-title">
           Сумма, которую запросил Ахмед
         </h3>
